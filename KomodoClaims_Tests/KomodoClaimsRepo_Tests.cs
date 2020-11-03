bdd550f2778429d476513fdc5d9e508bc11f30f5
ColinLittleSD <ColinLittleSD@gmail.com>
2020-11-03 09:01:05 -0500

    unit test

--- KomodoClaims_Tests/KomodoClaimsRepo_Tests.cs
+++ KomodoClaims_Tests/KomodoClaimsRepo_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using KomodoClaims_Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,6 +17,31 @@ namespace KomodoClaims_Tests
             repository.AddToQueueOfClaims(claim);
 
             Assert.AreEqual(1, repository.ClaimCount());
+        }
+
+        [TestMethod]
+        public void GetClaimsShouldReturn_QueueOfClaims()
+        {
+            Claim claim = new Claim();
+            Claim_Repo repo = new Claim_Repo();
+
+            repo.AddToQueueOfClaims(claim);
+
+            Queue<Claim> claims = repo.GetQueueOfClaims();
+            bool queueHasClaims = claims.Contains(claim);
+
+            Assert.IsTrue(queueHasClaims);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Claim claim = new Claim();
+            Claim_Repo repository = new Claim_Repo();
+
+            repository.HandleNextClaim();
+
+            Assert.AreEqual(0, repository.ClaimCount());
         }
     }
 }
